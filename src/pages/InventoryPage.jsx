56b58acc8bc9e9b2bbee799fc66161f1e081f339
Anggie <que23643@uvg.edu.gt>
2025-08-04 18:14:34 -0600

    topbar listo

--- src/pages/InventoryPage.jsx
+++ src/pages/InventoryPage.jsx
@@ -1,16 +1,18 @@
-
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import Topbar from '../components/Topbar'
 
 export default function InventoryPage() {
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       <Sidebar />
 
-      <div className="flex-1 p-6">
-        <h1 className="text-2xl font-semibold">Inventario</h1>
-        {/* Aquí puedes agregar el contenido de la página */}
-        <p>Bienvenido a la sección de inventario.</p>
+      <div className="flex-1 flex flex-col">
+        <Topbar />
+        <main className="p-6 flex-1 overflow-y-auto">
+          <h1 className="text-2xl font-semibold">Inventario</h1>
+          <p>Bienvenido a la sección de inventario.</p>
+        </main>
       </div>
     </div>
   )
